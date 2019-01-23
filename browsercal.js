0aa849b7db2fa25628860700795affbe8c50fb9e
israelone <itzisraelone@gmail.com>
2019-01-22 18:41:24 -0600

    Starting files

---
+++ browsercal.js
@@ -0,0 +1,22 @@
+function add(num, num1){
+    return num+num1;
+}
+
+function subtract(num, num1){
+    return num-num1;
+}
+
+function multiply(num, num1){
+    return num * num1;
+}
+
+function divide(num, num1){
+    return num / num1;
+}
+
+function operate(){
+
+    
+}
+
+console.log("we are linked");
