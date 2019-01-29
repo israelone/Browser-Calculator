c65fb5337771fe8d926296c17e012d51b40b5de8
israelone <itzisraelone@gmail.com>
2019-01-28 19:42:35 -0600

    Added some keys functionality

--- browsercal.js
+++ browsercal.js
@@ -1,3 +1,9 @@
+const numbers= Array.from(document.getElementsByClassName("numbers"));
+let inputScreen=document.getElementById("input");
+let operators=Array.from(document.getElementsByClassName("operations"));
+let equal=document.getElementById("operator");
+
+
 function add(num, num1){
     return num+num1;
 }
@@ -19,4 +25,29 @@ function operate(){
     
 }
 
-console.log("we are linked");
+numbers.forEach(function(e){
+    e.addEventListener("click", function(){
+        
+        inputScreen.textContent+=this.textContent;
+    })
+});
+
+operators.forEach(function(e){
+    e.addEventListener("click", function(){
+        let operation=e.innerHTML;
+        console.log("click", e.innerHTML);
+        let firstNumber=inputScreen.textContent;
+        inputScreen.textContent="";
+        console.log(typeof(firstNumber));
+        console.log(typeof(e));
+        
+    equal.addEventListener("click", function(){
+        let seconNumber=inputScreen.textContent;
+        console.log(firstNumber, seconNumber);
+        inputScreen.textContent="";
+        
+        inputScreen.textContent= firstNumber +e.innerHTML +seconNumber;
+    })
+        
+    })
+});
