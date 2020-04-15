8dc51064b51249d7955cfc45dab29ab9d3af0d8f
israel <itzisraelone@gmail.com>
2020-04-15 15:45:35 -0500

    Finish calculator functionality and design

--- browsercal.js
+++ browsercal.js
@@ -1,53 +1,183 @@
-const numbers= Array.from(document.getElementsByClassName("numbers"));
-let inputScreen=document.getElementById("input");
-let operators=Array.from(document.getElementsByClassName("operations"));
-let equal=document.getElementById("operator");
+let numbersButtons = Array.from(document.getElementsByClassName("numbers"));
+let displayScreen = document.getElementById("input");
+let preResultDisplay=document.getElementById("equation");
+let operatorsButtons = Array.from(document.getElementsByClassName("operations"));
+let equalButton = document.getElementById("operator");
+let clearButton = document.getElementById("clear");
+let mathArray = [];
+let orderOfOperation = [];
+let operatorsValues = [{symbol:"*", preference:3, associative:"left"}, {symbol:"/", preference:3, associative:"left"}, {symbol:"+", preference:2, associative:"left"}, {symbol:"-", preference:2, associative:"left"} ]
 
+//Listen for user input 
+numbersButtons.forEach(function(e){
 
-function add(num, num1){
-    return num+num1;
-}
+    e.addEventListener("click", function(){
+        
+        displayScreen.textContent += this.textContent;
+        preResultDisplay.textContent+=this.textContent;
 
-function subtract(num, num1){
-    return num-num1;
-}
-
-function multiply(num, num1){
-    return num * num1;
-}
-
-function divide(num, num1){
-    return num / num1;
-}
-
-function operate(){
-
+    })
     
+});
+
+operatorsButtons.forEach(function(e){
+
+    e.addEventListener("click", function(){
+
+        mathArray.push(parseInt(displayScreen.textContent));
+        preResultDisplay.textContent+=this.textContent;
+        displayScreen.textContent="";
+        mathArray.push(this.textContent);
+
+    })
+})  
+
+equalButton.addEventListener("click", function(){
+    
+    mathArray.push(parseInt(displayScreen.textContent));
+    
+    if(isNaN(mathArray[mathArray.length-1])){
+
+        displayScreen.textContent="Error";
+
+    }
+
+    else{
+    
+        preResultDisplay.textContent="";        
+        displayScreen.textContent=sortArray(mathArray);
+        mathArray.splice(0, mathArray.length);
+        preResultDisplay.textContent+=displayScreen.textContent;
+        orderOfOperation=[];
+
+    }
+       
+})
+
+clearButton.addEventListener("click", function(){
+
+    mathArray=[];
+    orderOfOperation=[];
+    preResultDisplay.textContent="";
+    displayScreen.textContent="";
+
+})
+
+
+//Iterates through user input and sort it using shunting yard algorithm
+function sortArray(){
+
+    let outputQueue = [];
+
+    for(var i = 0; i < mathArray.length; i++){
+
+        let inputPreference = 0;
+        let stackPreference = 0;
+
+        if(typeof(mathArray[i]) === "number"){
+
+            outputQueue.push(mathArray[i]);
+
+        }
+
+        else{
+
+            if(orderOfOperation.length-1>=0){ 
+                
+                //find operators preferences
+                stackPreference = operatorsValues.find(element => element.symbol === orderOfOperation[orderOfOperation.length-1]);
+                inputPreference = operatorsValues.find(element => element.symbol === mathArray[i]);
+    
+                    
+                if(stackPreference.preference < inputPreference.preference){
+                    
+                    orderOfOperation.push(mathArray[i]);
+                
+                }
+                
+                
+                else if(stackPreference.preference > inputPreference.preference){
+
+                        orderOfOperation.reverse();
+                        outputQueue.push(...orderOfOperation);
+                        orderOfOperation = [];
+                        orderOfOperation.push(mathArray[i]);
+
+                }
+
+                else if(stackPreference.preference === inputPreference.preference){
+
+                    outputQueue.push(orderOfOperation[orderOfOperation.length - 1]);
+                    orderOfOperation.splice(orderOfOperation.length-1, 1, mathArray[i]);
+
+                }
+
+            }
+
+            else {
+
+                orderOfOperation.push(mathArray[i]);
+
+            }
+
+        }
+
+    }  
+
+   orderOfOperation.reverse()
+   outputQueue = outputQueue.concat(orderOfOperation);
+   return calculator(outputQueue);
+
+} 
+
+//Calculates user input after it has been sorted
+function calculator(arr){
+
+    let stack=[];
+
+    for(var i = 0; i < arr.length; i++){
+
+        if(isNaN(arr[i])){
+
+            stack.splice(stack.length - 2, 2, calculate(stack[stack.length - 2], arr[i], stack[stack.length - 1]));
+        
+        }
+
+        else{
+
+          stack.push(arr[i]);
+
+        }
+
+    }
+
+    return stack;
+
 }
 
-numbers.forEach(function(e){
-    e.addEventListener("click", function(){
-        
-        inputScreen.textContent+=this.textContent;
-    })
-});
+function calculate(firstNumber, operator, secondNumber){
+    
+    switch(operator){
 
-operators.forEach(function(e){
-    e.addEventListener("click", function(){
-        let operation=e.innerHTML;
-        console.log("click", e.innerHTML);
-        let firstNumber=inputScreen.textContent;
-        inputScreen.textContent="";
-        console.log(typeof(firstNumber));
-        console.log(typeof(e));
+        case "*":
+        return Math.round(10*(firstNumber * secondNumber))/10;
+        break;
+
+        case "/":
+        return Math.round(10*(firstNumber / secondNumber))/10;
+        break;
+            
+        case "+":
+        return (firstNumber + secondNumber);
+        break;
+
+        case "-":
+        return (firstNumber - secondNumber);
+        break;
+
+    }
         
-    equal.addEventListener("click", function(){
-        let seconNumber=inputScreen.textContent;
-        console.log(firstNumber, seconNumber);
-        inputScreen.textContent="";
-        
-        inputScreen.textContent= firstNumber +e.innerHTML +seconNumber;
-    })
-        
-    })
-});
+}
+   
+ 
+
